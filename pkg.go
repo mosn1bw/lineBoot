d2def5b6add7e812a73a74abdc178528c3fd2bfc
patrice <keroro0403@gmail.com>
2018-02-04 12:33:46 +0800

    add game time

--- pkg.go
+++ pkg.go
@@ -1,6 +1,11 @@
 package main
 
-import "strings"
+import (
+	"log"
+	"strconv"
+	"strings"
+	"time"
+)
 
 func ParseReceiveMsg(msg string) string {
 	var recMsg string
@@ -14,4 +19,16 @@ func LeftPad2Len(s string, padStr string, overallLen int) string {
 	padCountInt = 1 + ((overallLen - len(padStr)) / len(padStr))
 	var retStr = strings.Repeat(padStr, padCountInt) + s
 	return retStr[(len(retStr) - overallLen):]
+}
+
+func UtcMillis2TimeString(utcMillisStr string, timeFormat string) string {
+	utcMillis, err := strconv.ParseInt(utcMillisStr, 10, 64)
+	if err != nil {
+		log.Printf("parse time error: %v", err)
+		return ""
+	}
+	utcTimestamp := utcMillis / 1000
+	gameTime := time.Unix(utcTimestamp, 0)
+	gameTimeStr := gameTime.Format(timeFormat)
+	return gameTimeStr
 }
